6fb30d825378e746a7a95cb7b333056bcc6e6282
Mateusz Gajdzik <mgajdzik@gmail.com>
2021-12-23 22:48:08 +0100

    Added DynamicTickingComponent that can be added dynamically from (for instance) gameplay ability to enable ticking (by event dispatcher)

---
+++ Source/GameplayUtilities/Private/Components/DynamicTickingComponent.cpp
@@ -0,0 +1,38 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Components/DynamicTickingComponent.h"
+
+
+void UDynamicTickingComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                             FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	OnComponentTicked.Broadcast(DeltaTime);
+}
+
+UDynamicTickingComponent* UDynamicTickingComponent::GetDynamicComponentFromActor(AActor* Actor,
+	TSubclassOf<UDynamicTickingComponent> ComponentClass)
+{
+	if (!IsValid(Actor)) return nullptr;
+	TArray<UDynamicTickingComponent*> Components;
+	Actor->GetComponents(Components);
+	UDynamicTickingComponent* Result = nullptr;
+	for (UDynamicTickingComponent* Component : Components)
+	{
+		if (Component->GetClass() == ComponentClass)
+		{
+			Result = Component;
+			break;
+		}		
+	}
+	
+	if (!IsValid(Result))
+	{
+		Result = NewObject<UDynamicTickingComponent>(Actor, ComponentClass);
+		if (!IsValid(Result)) return nullptr;
+		Result->RegisterComponent();
+	}
+	return Result;
+}
+
---
+++ Source/GameplayUtilities/Public/Components/DynamicTickingComponent.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DynamicTickingComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnComponentTicked, float, DeltaTime);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class GAMEPLAYUTILITIES_API UDynamicTickingComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	explicit UDynamicTickingComponent(const FObjectInitializer& ObjectInitializer)
+		: Super(ObjectInitializer)
+	{
+		PrimaryComponentTick.bCanEverTick = true;
+	}
+
+	UDynamicTickingComponent() : Super()
+	{
+		PrimaryComponentTick.bCanEverTick = true;
+	}
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+	
+
+	UFUNCTION(BlueprintCallable)
+	static UDynamicTickingComponent* GetDynamicComponentFromActor(AActor* Actor, TSubclassOf<UDynamicTickingComponent> ComponentClass);
+
+	UPROPERTY(BlueprintAssignable)
+	FOnComponentTicked OnComponentTicked;
+};
+
+
